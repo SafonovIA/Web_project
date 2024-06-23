37b568337da0434f9ed22e35d4b35631375e5b3a
SafonovIA <safonov.gosha2016@yandex.ru>
2024-06-23 15:02:48 +0300

    Изменен main.py, добавлены страницы регистрации и корзины изменено название index.html на home_page.html

--- source/main.py
+++ source/main.py
@@ -1,14 +1,27 @@
 from flask import Flask, render_template
 
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route("/")
-    def index():
-        page_title = "Интернет магазин"
-        return render_template("index.html", page_title=page_title)
-    return app
+
+@app.route("/")
+def home_page():
+    page_title = "Интернет магазин"
+    return render_template("home_page.html", page_title=page_title)
+
+
+@app.route("/login")
+def login():
+    page_title = "Авторизация"
+
+    return render_template("login.html", page_title=page_title)
+
+
+@app.route("/basket")
+def basket():
+    page_title = "Корзина"
+    return render_template("basket.html", page_title=page_title)
+
 
 if __name__ == "__main__":
-    create_app().run(debug=True)
+    app.run(debug=True)
