aa930ff483c4b3f575fb7c34870153c8b4fe0c7b
Maxeow <sparjaa@gmail.com>
2024-07-02 05:44:38 +0300

    поправил

--- source/main.py
+++ source/main.py
@@ -10,22 +10,20 @@ def home_page():
     main_img = 'https://sun9-67.userapi.com/c844721/v844721600/12813f/IOTsEqFrmZY.jpg'
     #текст под картинкой
     text1 = 'Выберете интересующий вас раздел' 
-    #кнопки
+    #кнопки c гендерами
     button1 = 'Мужское'
     button2 = 'Женское'
     button3 = 'Детское'
-    
-    tshirts = Tshirt.query.all()
-    sweatwears = Sweatwear.query.all()
-    outwers = Outwear.query.all()
-    socks = Socks.query.all()
-    
+    #под кнопками будут отображаться рандомные карточки товаров
+    tshirts = Tshirt.query.add_columns(Tshirt.picture, Tshirt.type_item, Tshirt.price).all()
+    sweatwears = Sweatwear.query.add_columns(Sweatwear.picture, Sweatwear.type_item, Sweatwear.price).all()
+    outwears = Outwear.query.add_columns(Outwear.picture, Outwear.type_item, Outwear.price).all()
+    socks = Socks.query.add_columns(Socks.picture, Socks.type_item, Socks.price).all()
+    shoes = Shoes.query.add_columns(Shoes.picture, Shoes.type_item, Shoes.price).all()
+    trousers = Trousers.query.add_columns(Trousers.picture, Trousers.type_item, Trousers.price).all()
     products = []
     
-    products.extend(tshirts)
-    products.extend(sweatwears)
-    products.extend(outwers)
-    products.extend(socks)
+    products = tshirts + sweatwears + outwears + socks + shoes + trousers
     
     random.shuffle(products)
     products = products[:4]
@@ -44,17 +42,34 @@ def basket():
     page_title = "Корзина"
     return render_template("basket.html", page_title=page_title)
 
+# кнопки на главной странице с гендерным фильтром
 @app.route("/<gender>", methods = ['GET'])
-def catalo_gender(gender):
+def catalog_gender(gender):
+    # показывает только мужское
     if gender == 'mans':
         page_title = 'Мужское'
+        tshirts = Tshirt.query.add_columns(Tshirt.picture, Tshirt.type_item, Tshirt.price)\
+                          .filter(Tshirt.gender == 'мужской').all()
+        sweatwears = Sweatwear.query.add_columns(Sweatwear.picture, Sweatwear.type_item, Sweatwear.price)\
+                                .filter(Sweatwear.gender == 'мужской').all()
+        outwears = Outwear.query.add_columns(Outwear.picture, Outwear.type_item, Outwear.price)\
+                            .filter(Outwear.gender == 'мужской').all()
+        socks = Socks.query.add_columns(Socks.picture, Socks.type_item, Socks.price)\
+                       .filter(Socks.gender == 'мужской').all()
+        shoes = Shoes.query.add_columns(Shoes.picture, Shoes.type_item, Shoes.price)\
+                       .filter(Shoes.gender == 'мужской').all()
+        trousers = Trousers.query.add_columns(Trousers.picture, Trousers.type_item, Trousers.price)\
+                             .filter(Trousers.gender == 'мужской').all()
+
+    
+        products = tshirts + sweatwears + outwears + socks + shoes + trousers
+    # показывает только женское
     elif gender == 'womans':
         page_title = 'Женское'
     else:
         page_title = 'Детское'
+    return render_template('catalog.html', page_title=page_title, products=products)
     
-    return render_template('catalog.html', page_title=page_title)
-    
-
+ 
 if __name__ == "__main__":
     app.run(debug=True)
