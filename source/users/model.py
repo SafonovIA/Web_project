a9b143fddc9f477d3ab431a686a4290db8765f95
SafonovIA <safonov.gosha2016@yandex.ru>
2024-07-05 21:22:30 +0300

    Добавлен функционал авторизации пользователей

---
+++ source/users/model.py
@@ -0,0 +1,24 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from config import db
+
+
+class User_account(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), index=True, unique=True,
+                         nullable=False)
+    password = db.Column(db.String, nullable=False)
+    role = db.Column(db.String(10), index=True)
+
+    def __repr__(self) -> str:
+        return f"User {self.user_name}, id {self.id}"
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
---
+++ source/users/views.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, render_template, flash, redirect, url_for
+from users.model import User_account
+from users.forms import LoginForm
+from flask_login import login_user, logout_user, current_user
+
+
+blueprint = Blueprint('user', __name__, url_prefix='/users')
+
+
+@blueprint.route("/login")
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('account'))
+    login_form = LoginForm()
+    return render_template("login.html", form=login_form)
+
+
+@blueprint.route('/process-login', methods=['POST'])
+def process_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User_account.query.filter_by(
+            username=form.username.data
+            ).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            flash('Вы вошли на сайт')
+            return redirect(url_for('account'))
+    flash('Неправильное имя пользователя или пароль')
+    return redirect(url_for('user.login'))
+
+
+@blueprint.route('/logout')
+def logout():
+    logout_user()
+    flash("Вы успешно вышли")
+    return redirect(url_for('home_page'))
