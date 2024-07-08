882ff37500550816e8786d0af2ed1b2b0a6bd23e
SafonovIA <safonov.gosha2016@yandex.ru>
2024-07-08 19:53:29 +0300

    Поправил Blueprint

--- source/home_page/views.py
+++ source/home_page/views.py
@@ -35,3 +35,35 @@ def home_page():
     products = products[:4]
 
     return render_template("home_page.html", products=products)
+
+
+# кнопки на главной странице с гендерным фильтром
+@blueprint.route("/<gender>", methods=['GET'])
+def catalog_gender(gender):
+    # показывает только мужское
+    if gender == 'mans':
+        tshirts = Tshirt.query.add_columns(
+            Tshirt.picture, Tshirt.type_item, Tshirt.price
+            ).filter(Tshirt.gender == 'мужской').all()
+        sweatwears = Sweatwear.query.add_columns(
+            Sweatwear.picture, Sweatwear.type_item, Sweatwear.price
+            ).filter(Sweatwear.gender == 'мужской').all()
+        outwears = Outwear.query.add_columns(
+            Outwear.picture, Outwear.type_item, Outwear.price
+            ).filter(Outwear.gender == 'мужской').all()
+        socks = Socks.query.add_columns(
+            Socks.picture, Socks.type_item, Socks.price
+            ).filter(Socks.gender == 'мужской').all()
+        shoes = Shoes.query.add_columns(
+            Shoes.picture, Shoes.type_item, Shoes.price
+            ).filter(Shoes.gender == 'мужской').all()
+        trousers = Trousers.query.add_columns(
+            Trousers.picture, Trousers.type_item, Trousers.price
+            ).filter(Trousers.gender == 'мужской').all()
+
+        products = tshirts + sweatwears + outwears + socks + shoes + trousers
+    # показывает только женское
+        return render_template('catalog.html', products=products)
+
+    elif gender == 'womans':
+        pass
--- source/main.py
+++ source/main.py
@@ -7,6 +7,7 @@ from admin.views import blueprint as admin_blueprint
 from users.views import blueprint as user_blueprint
 from home_page.views import blueprint as home_page_blueprint
 
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'user.login'
@@ -24,40 +25,6 @@ def load_user(user_id):
 def basket():
     page_title = "Корзина"
     return render_template("basket.html", page_title=page_title)
-
-
-"""
-# кнопки на главной странице с гендерным фильтром
-@app.route("/<gender>", methods=['GET'])
-def catalog_gender(gender):
-    # показывает только мужское
-    if gender == 'mans':
-        tshirts = Tshirt.query.add_columns(
-            Tshirt.picture, Tshirt.type_item, Tshirt.price
-            ).filter(Tshirt.gender == 'мужской').all()
-        sweatwears = Sweatwear.query.add_columns(
-            Sweatwear.picture, Sweatwear.type_item, Sweatwear.price
-            ).filter(Sweatwear.gender == 'мужской').all()
-        outwears = Outwear.query.add_columns(
-            Outwear.picture, Outwear.type_item, Outwear.price
-            ).filter(Outwear.gender == 'мужской').all()
-        socks = Socks.query.add_columns(
-            Socks.picture, Socks.type_item, Socks.price
-            ).filter(Socks.gender == 'мужской').all()
-        shoes = Shoes.query.add_columns(
-            Shoes.picture, Shoes.type_item, Shoes.price
-            ).filter(Shoes.gender == 'мужской').all()
-        trousers = Trousers.query.add_columns(
-            Trousers.picture, Trousers.type_item, Trousers.price
-            ).filter(Trousers.gender == 'мужской').all()
-
-        products = tshirts + sweatwears + outwears + socks + shoes + trousers
-    # показывает только женское
-        return render_template('catalog.html', products=products)
-
-    elif gender == 'womans':
-        pass
-"""
 
 
 if __name__ == "__main__":
